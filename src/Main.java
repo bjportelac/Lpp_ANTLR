a0ef03e0ed201618a733ab3cf53b00691a6b6bdc
Brayan Portela <32397114+bjportelac@users.noreply.github.com>
2023-11-07 00:02:54 -0500

    Update Main.java
    Removed unnecessary command

--- src/Main.java
+++ src/Main.java
@@ -20,11 +20,7 @@ public class Main {
 
             scanner.close();
             code.clear();
-
-            try {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } catch (Exception e) {}
-
+            
             LPP_grammarLexer lexer = new LPP_grammarLexer(CharStreams.fromString(stringProgram));
 
             CommonTokenStream tokenStream = new CommonTokenStream(lexer);
